7e780cc6f5d7dc7cea1c8b94d38b245744785e64
mschoenebeck <matthias.schoenebeck@gmail.com>
2025-01-05 11:11:17 -0500

    write out VK separately as little-endian hex string

--- src/bin/split_params.rs
+++ src/bin/split_params.rs
@@ -4,9 +4,11 @@ extern crate phase2;
 extern crate pairing;
 extern crate rand;
 extern crate blake2_rfc;
+extern crate zeos_caterpillar;
 
 use std::fs::File;
 use std::io::{BufWriter, BufReader};
+use zeos_caterpillar::contract::AffineVerifyingKeyBytesLE;
 
 fn main() {
     let current_params = File::open("params").expect("couldn't open `./params`");
@@ -21,23 +23,39 @@ fn main() {
         let f = File::create("mint.params").expect("couldn't create `./mint.params`");
         let mut f = BufWriter::with_capacity(1024*1024, f);
         mint.write(&mut f).expect("couldn't write new Mint params");
+        // write out verifying key as little-endian encoded hex string for easy upload to the smart contract
+        let vk_affine_bytes = AffineVerifyingKeyBytesLE::from(mint.get_params().vk.clone());
+        let res = std::fs::write("mint.vk.hex", hex::encode(vk_affine_bytes.0));
+        assert!(res.is_ok());
     }
 
     {
         let f = File::create("spend-output.params").expect("couldn't create `./spend-output.params`");
         let mut f = BufWriter::with_capacity(1024*1024, f);
         spend_output.write(&mut f).expect("couldn't write new SpendOutput params");
+        // write out verifying key as little-endian encoded hex string for easy upload to the smart contract
+        let vk_affine_bytes = AffineVerifyingKeyBytesLE::from(spend_output.get_params().vk.clone());
+        let res = std::fs::write("spend-output.vk.hex", hex::encode(vk_affine_bytes.0));
+        assert!(res.is_ok());
     }
 
     {
         let f = File::create("spend.params").expect("couldn't create `./spend.params`");
         let mut f = BufWriter::with_capacity(1024*1024, f);
         spend.write(&mut f).expect("couldn't write new Spend params");
+        // write out verifying key as little-endian encoded hex string for easy upload to the smart contract
+        let vk_affine_bytes = AffineVerifyingKeyBytesLE::from(spend.get_params().vk.clone());
+        let res = std::fs::write("spend.vk.hex", hex::encode(vk_affine_bytes.0));
+        assert!(res.is_ok());
     }
 
     {
         let f = File::create("output.params").expect("couldn't create `./output.params`");
         let mut f = BufWriter::with_capacity(1024*1024, f);
         output.write(&mut f).expect("couldn't write new Output params");
+        // write out verifying key as little-endian encoded hex string for easy upload to the smart contract
+        let vk_affine_bytes = AffineVerifyingKeyBytesLE::from(output.get_params().vk.clone());
+        let res = std::fs::write("output.vk.hex", hex::encode(vk_affine_bytes.0));
+        assert!(res.is_ok());
     }
 }
